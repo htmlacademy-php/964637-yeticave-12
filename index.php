eb5a5991598ec1f1d2667210eb7f38a8024b3d84
LickurG <iamlickurg@gmail.com>
2021-12-17 22:33:26 +0700

    Итерация по массивам

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
 <?php
 $is_auth = rand(0, 1);
+$user_name = 'Артем'; 
 
-$user_name = 'Артем'; // укажите здесь ваше имя
+$categories = [
+    'Доски и лыжи' => [
+            'title' => '2014 Rossignol District Snowboard',
+            'price' => '10999',
+            'URL photo' => 'img/lot-1.jpg',
+    ],
+    'Крепления' => [
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'price' => '8000',
+        'URL photo' => 'img/lot-3.jpg',
+    ],
+    'Ботинки' => [
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'price' => '10999',
+        'URL photo' => 'img/lot-4.jpg',
+    ],
+    'Одежда' => [
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'price' => '7500',
+        'URL photo' => 'img/lot-5.jpg',
+    ],
+    'Инструменты' => [
+        'title' => '',
+        'price' => '',
+        'URL photo' => '',
+    ],
+    'Разное' => [
+        'title' => 'Маска Oakley Canopy',
+        'price' => '5400',
+        'URL photo' => 'img/lot-6.jpg',
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -52,10 +84,12 @@ $user_name = 'Артем'; // укажите здесь ваше имя
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
+            <?php foreach ($categories as $key => $value): ?>
             <!--заполните этот список из массива категорий-->
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$key?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -63,18 +97,19 @@ $user_name = 'Артем'; // укажите здесь ваше имя
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach ($categories as $key => $value): ?>
             <!--заполните этот список из массива с товарами-->
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$key?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['title']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__amount"><?=$value['price']?></span>
+                            <span class="lot__cost"><?=$value['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -82,6 +117,7 @@ $user_name = 'Артем'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -90,10 +126,12 @@ $user_name = 'Артем'; // укажите здесь ваше имя
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
+            <?php foreach ($categories as $key => $value): ?>
             <!--заполните этот список из массива категорий-->
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$key?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
