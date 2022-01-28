81874e17c6378d34d5aafe2d8ef2abde950a581d
LickurG <iamlickurg@gmail.com>
2022-01-28 16:36:42 +0700

    Работаем с датой

--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
         <?php foreach ($categories as $value): ?>
         <!--заполните этот список из массива категорий-->
         <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($value);?></a>
+            <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($value); ?></a>
         </li>
         <?php endforeach; ?>
     </ul>
@@ -22,17 +22,17 @@
                 <img src="" width="350" height="260" alt="">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?=htmlspecialchars($value['category']);?></span>
+                <span class="lot__category"><?=htmlspecialchars($value['category']); ?></span>
                 <h3 class="lot__title">
-                    <a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['title']);?></a>
+                    <a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['title']); ?></a>
                 </h3>
                 <div class="lot__state">
                     <div class="lot__rate">
-                        <span class="lot__amount"><?htmlspecialchars($value['price']);?></span>
-                        <span class="lot__cost"><?= formatPrice(htmlspecialchars($value['price'])); ?></span>
+                        <span class="lot__amount"><?=htmlspecialchars($value['price']); ?></span>
+                        <span class="lot__cost"><?=formatPrice(htmlspecialchars($value['price']));?></span>
                     </div>
-                    <div class="lot__timer timer">
-                        12:23
+                    <div class="lot__timer timer <?=timerFinishing($value['completion date']); ?>">
+                        <?=get_dt_range($value['completion date']); ?>
                     </div>
                 </div>
             </div>
